0fd1b4786003414f865471602a3a4c3bda2549a6
Geoff Garbers <geoff@garbers.co.za>
2019-10-03 09:05:30 +0200

    Add push script.

---
+++ push.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+project="$1"
+hub_repo_name="$2"
+
+# Ensure we have a project id.
+if [[ "$#" -lt 2 ]]; then
+    printf "Usage: $0 \e[4mGCP_PROJECT\e[0m \e[4mHUB_REPO_NAME\e[0m\n"
+    exit 1
+fi
+
+# Change into this directory.
+cd "$(cd "$(dirname "$0")" && pwd)"
+
+# Submit the build.
+printf "\n\e[38;5;116mSubmitting build.\e[0m\n"
+gcloud builds submit \
+    --project "$project" \
+    --substitutions "_DOCKER_HUB_REPO=${hub_repo_name}" \
+    --config ./cloudbuild.yaml .
